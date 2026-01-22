d374bd659c7256f0a9c628e6ab5a1b8bf1d3905e
Sungmin Kim <sk3378@princeton.edu>
2026-01-21 19:42:16 -0500

    update: added second method for computing matrix mul using masked_fill and softmax

--- self_attention_exploration.py
+++ self_attention_exploration.py
@@ -11,7 +11,7 @@ x = torch.randn(B,T,C)
 print(x.shape)
 
 # quick test with torch.mean()
-a = torch.randn(2, 4) # t = 2, C = 4
+a = torch.randn(2, 4) # t = 2, C = 4 
 print(f"a: {a}")
 print(f"mean 0: {torch.mean(a, 0)}") # mean across dimension 0... aka across time -> leaves (C) tensor
 print(f"mean 1: {torch.mean(a, 1)}") # mean across dimension 1... aka across channel -> leaves (t) tensor
@@ -29,8 +29,8 @@ for b in range(B):
 print(f"original x[0]:\n{x[0]}")
 print(f"bow average x[0]:\n{xbow[0]}")
 
-# using a mathematical trick, we can try to be very EFFICIENT with this average computation, instead of nested loops
-# NOTE: with the use of lower-half triangular matrix of normalized row sum to 1, matrix multiplication produces a running average
+# Below, we will try to reproduce the above results in more efficient ways, w/ math trick instead of nested loops
+# NOTE: with the use of lower triangular matrix of normalized row sum to 1, matrix multiplication produces a running average
 # see example below for clarity
 a = torch.tril(torch.ones(3, 3))
 a = a / torch.sum(a, 1, keepdim=True) # dividng by sum across dim 1 gives each row normalized to 1
@@ -39,3 +39,21 @@ c = a @ b # matrix multiply
 print(f"a:\n{a}")
 print(f"b:\n{b}")
 print(f"c:\n{c}")
+
+# now actually make the previous computation efficient
+# approach 1) lower triangular matrix then normalizing rows
+w = torch.tril(torch.ones(T, T)) # the dimensions should be T
+w = w / w.sum(1, keepdim=True)
+print(f"weights:\n{w}")
+# NOTE: since (T, T) @ (B, T, C) isn't compatible in dimensions, PyTorch inserts a third batch dim: (T, T) -> (B, T, T)
+xbow2 = w @ x # this becomes a batched (T, T) @ (T, C) multiplication -> (B, T, C)
+print(f"is xbow and xbow2 identical? {torch.allclose(xbow, xbow2)}") # this compares the two tensors, we expect TRUE if identical
+
+# approach 2) back-filling the weight matrix w/ masked_fill() and softmax()
+tril = torch.tril(torch.ones(T, T)) # create a lower triangular matrix
+w2 = torch.zeros((T, T))
+w2 = w2.masked_fill(tril == 0, float('-inf')) # find where values are 0, replace with -inf
+w2 = F.softmax(w2, dim=1) # by taking softmax, we are calculating the same normalized row values (via exponentiating and normalizing each row)
+xbow3 = w2 @ x # we should expect the same output
+print(f"is xbow and xbow3 identical? {torch.allclose(xbow, xbow3)}")
+# NOTE: we actually prefer approach 2 because it simulates self-attention more accurately: by back-filling -inf values with a seed, the model is holding attention to past tokens and interacting in different weights.
